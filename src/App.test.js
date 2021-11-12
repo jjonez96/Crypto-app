c1e4b3a435400f8a16f1daadc9ee31fac92a3fbf
timojkankaanpaa <tka@puv.fi>
2021-11-12 22:39:30 +0200

    Fixed enzyme version 16->17

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from "./App";
 import { configure, shallow, assert } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 configure({ adapter: new Adapter() });
 describe('The text is correct")', () => {
